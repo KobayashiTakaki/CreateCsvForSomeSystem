50e79b4bd4b85941919004270e5aafd411bc14e8
kobayashitakaki <takaki@takakisan.com>
2018-12-09 13:55:45 +0900

    refactor opening file process with "with" block

--- CreateCsvForSomeSystem.py
+++ CreateCsvForSomeSystem.py
@@ -47,132 +47,131 @@ try:
 
     output_file = os.path.join(output_dir, year + '_' + month + '.csv')
 
-    f = open(output_file, mode='w', encoding='shift_jis')
+    with open(output_file, mode='w', encoding='shift_jis') as f:
 
-    #csvファイルにヘッダーを書き込み
-    f.write(csv_header)
-    f.write('\r\n')
-
-    rows = soup.find(class_="htBlock-adjastableTableF_inner").find("table").find("tbody").find_all("tr")
-    for row in rows:
-        #出勤が空だったらcontinue
-        if len(row.find(attrs={"data-ht-sort-index" : "START_TIMERECORD"}).find("p").text.strip()) == 0:
-            continue
-
-        #1
-        #日付
-        td = row.find(attrs={"data-ht-sort-index" : "WORK_DAY"})
-        if td is not None:
-            date = td.find("p").text.strip()
-            f.write(util.format_date(year, date))
-        f.write(",")
-
-        #2
-        #オーダーNo.
-        f.write(order_no)
-        f.write(",")
-
-        #3
-        #オーダー名称
-        f.write(",")
-
-        #4
-        #作業フェーズNo.
-        f.write(phase_no)
-        f.write(",")
-
-        #5
-        #作業フェーズ名称
-        f.write(",")
-
-        #6
-        #工数
-        td = row.find(attrs={"data-ht-sort-index" : "ALL_WORK_MINUTE"})
-        if td is not None:
-            time = td.find("p").text.strip()
-            f.write(util.conv_mins_to_quarter(time.split('.')[0], time.split('.')[1]))
-        f.write(",")
-
-        #7
-        #摘要コード
-        f.write(",")
-
-        #8
-        #摘要名
-        f.write(",")
-
-        #9
-        #分類１_階層１コード
-        f.write(",")
-
-        #10
-        #分類１_階層２コード
-        f.write(",")
-
-        #11
-        #分類１_階層１名称
-        f.write(",")
-
-        #12
-        #分類１_階層２名称
-        f.write(",")
-
-        #13
-        #分類２_階層１コード
-        f.write(",")
-
-        #14
-        #分類２_階層２コード
-        f.write(",")
-
-        #15
-        #分類２_階層１名称
-        f.write(",")
-
-        #16
-        #分類２_階層２名称
-        f.write(",")
-
-        #17
-        #備考
-        f.write(",")
-
-        #18
-        #勤怠情報ー開始時刻
-        td = row.find(attrs={"data-ht-sort-index" : "START_TIMERECORD"})
-        if td is not None:
-            #p要素の中にspan要素があったら削除する
-            if td.find("p").find("span") is not None:
-                spans = td.find("p").find_all("span")
-                for span in spans:
-                    span.extract()
-
-            f.write(td.find("p").text.strip())
-        f.write(",")
-
-        #19
-        #勤怠情報ー終了時刻
-        td = row.find(attrs={"data-ht-sort-index" : "END_TIMERECORD"})
-        if td is not None:
-            #p要素の中にspan要素があったら削除する
-            if td.find("p").find("span") is not None:
-                spans = td.find("p").find_all("span")
-                for span in spans:
-                    span.extract()
-
-            f.write(td.find("p").text.strip())
-        f.write(",")
-
-        #20
-        #勤怠情報ー休暇時間
-        td = row.find(attrs={"data-ht-sort-index" : "REST_MINUTE"})
-        if td is not None:
-            time = td.find("p").text.strip()
-            f.write(util.calc_not_work_time(time.split('.')[0], time.split('.')[1]))
-
-        #改行
+        #csvファイルにヘッダーを書き込み
+        f.write(csv_header)
         f.write('\r\n')
+
+        rows = soup.find(class_="htBlock-adjastableTableF_inner").find("table").find("tbody").find_all("tr")
+        for row in rows:
+            #出勤が空だったらcontinue
+            if len(row.find(attrs={"data-ht-sort-index" : "START_TIMERECORD"}).find("p").text.strip()) == 0:
+                continue
+
+            #1
+            #日付
+            td = row.find(attrs={"data-ht-sort-index" : "WORK_DAY"})
+            if td is not None:
+                date = td.find("p").text.strip()
+                f.write(util.format_date(year, date))
+            f.write(",")
+
+            #2
+            #オーダーNo.
+            f.write(order_no)
+            f.write(",")
+
+            #3
+            #オーダー名称
+            f.write(",")
+
+            #4
+            #作業フェーズNo.
+            f.write(phase_no)
+            f.write(",")
+
+            #5
+            #作業フェーズ名称
+            f.write(",")
+
+            #6
+            #工数
+            td = row.find(attrs={"data-ht-sort-index" : "ALL_WORK_MINUTE"})
+            if td is not None:
+                time = td.find("p").text.strip()
+                f.write(util.conv_mins_to_quarter(time.split('.')[0], time.split('.')[1]))
+            f.write(",")
+
+            #7
+            #摘要コード
+            f.write(",")
+
+            #8
+            #摘要名
+            f.write(",")
+
+            #9
+            #分類１_階層１コード
+            f.write(",")
+
+            #10
+            #分類１_階層２コード
+            f.write(",")
+
+            #11
+            #分類１_階層１名称
+            f.write(",")
+
+            #12
+            #分類１_階層２名称
+            f.write(",")
+
+            #13
+            #分類２_階層１コード
+            f.write(",")
+
+            #14
+            #分類２_階層２コード
+            f.write(",")
+
+            #15
+            #分類２_階層１名称
+            f.write(",")
+
+            #16
+            #分類２_階層２名称
+            f.write(",")
+
+            #17
+            #備考
+            f.write(",")
+
+            #18
+            #勤怠情報ー開始時刻
+            td = row.find(attrs={"data-ht-sort-index" : "START_TIMERECORD"})
+            if td is not None:
+                #p要素の中にspan要素があったら削除する
+                if td.find("p").find("span") is not None:
+                    spans = td.find("p").find_all("span")
+                    for span in spans:
+                        span.extract()
+
+                f.write(td.find("p").text.strip())
+            f.write(",")
+
+            #19
+            #勤怠情報ー終了時刻
+            td = row.find(attrs={"data-ht-sort-index" : "END_TIMERECORD"})
+            if td is not None:
+                #p要素の中にspan要素があったら削除する
+                if td.find("p").find("span") is not None:
+                    spans = td.find("p").find_all("span")
+                    for span in spans:
+                        span.extract()
+
+                f.write(td.find("p").text.strip())
+            f.write(",")
+
+            #20
+            #勤怠情報ー休暇時間
+            td = row.find(attrs={"data-ht-sort-index" : "REST_MINUTE"})
+            if td is not None:
+                time = td.find("p").text.strip()
+                f.write(util.calc_not_work_time(time.split('.')[0], time.split('.')[1]))
+
+            #改行
+            f.write('\r\n')
 
 finally:
     driver.quit()
-    f.close()
